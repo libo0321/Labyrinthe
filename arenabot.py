1339a1ec6937802af9a236c16c8f0088655f4d53
ClaireQ <nnnnnkk1997@gmail.com>
2019-06-14 10:33:00 +0200

    generate1

--- arenabot.py
+++ arenabot.py
@@ -3,7 +3,7 @@
 
 import sys
 import random
-import numpy
+import numpy as np
 
 '''This function accepts in input a list of strings, and tries to parse them to update the position of a robot. Then returns distance from objective.'''
 def fitnessRobot(listOfCommands, visualize=False) :
@@ -73,6 +73,65 @@ def fitnessRobot(listOfCommands, visualize=False) :
     
     return distanceFromObjective
 
+
+def tournamentSelection(population, size):
+    tourn=np.random.choice(population,size, replace=False) # Selection du nombre d'individu pour le tournoi
+    tournSorted=sorted(tourn, key=lambda k: k['Fitness']) # Classement des individus selon la fitness
+    return tournSorted[0] # Renvoi du meilleur individu
+
+
+def croisement(genome1, genome2):
+    n_commands = len(genome1)
+    pCros = random.uniforme(0, 1)
+    for i in range(n_commands):
+        ifCros = random.uniforme(0, 1)
+        if ifCros<pCros:
+            genome1[i] = genome2[i]
+    return genome1
+
+# can't change frome rotate to move or from move to rotate
+def mutation(genome, mu, tau_move, tau_rotation):
+    n_commands = len(genome)
+    for i in range(n_commands):
+        pMu = random.uniforme(0, 1)
+        if pMu<mu:
+            if genome[i][0]=='r':
+                angle = filter(str.isdigit, genome[i])
+                angle = angle + random.randint(-tau_rotation, tau_rotation)
+                genome[i] = 'rotate' + str(angle)
+            else:
+                length = filter(str.isdigit, genome[i])
+                length = length + randint(-tau_move, tau_move)
+                length = max(0, min(length, 90))
+                genome[i] = 'move' + length
+    return genome
+
+'''This function will generate the next generation with the first method: mu-lambda best parents are conserved, and lambda new children are produced.'''
+def nextGeneration_1(population, popSize, p_croisement, p_enfant):
+    n_enfant = int(popSize * p_enfant)
+    n_parent = popSize - n_enfant
+    mergedPopulation = population[0:n_parent]
+    n_croisement = int(n_enfant * p_croisement)
+    n_mutation = n_enfant - n_croisement
+    tournSize = 2 # Taille de tournoi
+    # generate children by croisement
+    for i in range(n_croisement):
+        newIndividual = {}
+        # Selection de l'individu à croiser
+        indv1 = tournamentSelection(population, tournSize)
+        indv2 = tournamentSelection(population, tournSize)
+        newIndividual['Genome'] = croisement(indv1['Genome'], indv2['Genome'])
+        newIndividual['Fitness'] = fitnessRobot(newIndividual['Genome'], False)
+        mergedPopulation.append(newIndividual)
+    for i in range(n_mutation):
+        indv = tournamentSelection(population, tournSize)
+        newIndividual = {}
+        newIndividual['Genome'] = mutation(indv['Genome'], 0.3, 20, 10)
+        newIndividual['Fitness'] = fitnessRobot(newIndividual['Genome'], False)
+        mergedPopulation.append(newIndividual)
+    return mergedPopulation
+
+
 ################# MAIN
 def main() :
 	
@@ -80,7 +139,7 @@ def main() :
     # listOfCommands = []
     # fitnessRobot(listOfCommands, visualize=True)
     
-    popSize = 20 # population
+    popSize = 50 # population
     n_commands = 20 # length of the command list of each person
     n_rotate = 0.2 # proportion of rotate commands
     n_gen = 20 # number of generations
@@ -91,18 +150,20 @@ def main() :
         newIndividual = {}
         commands = []
         for j in range(n_commands):
-            n_random = random(1, 100)
-            if n_random<=n_random*n_rotate:
-                command[] = 'rotate' + str(random.randint(-90, 90))
+            n_random = random.randint(1, 100)
+            if n_random<=100*n_rotate:
+                # rotate angle should be between -90 and 90
+                command = 'rotate' + str(random.randint(-90, 90))
             else:
+                # move length should be between 1 and 90
                 command = 'move' + str(random.randint(1,90))
             commands.append(command)
-        newIndividual['Genome'] = commmands
+        newIndividual['Genome'] = commands
         newIndividual['Fitness'] = fitnessRobot(commands, False)
         population.append(newIndividual)
     
     #Tri de la population en fonction de la Fitness des individus
-    population = sorted(population, key=lambda k:k['Fitness'], reverse=Direction=='Minimisation')
+    population = sorted(population, key=lambda k:k['Fitness'])
     
     #Calcul des informations sur la population : meilleure fitness, fitness moyenne, pire fitness et déviation standard des fitness
     fitnesses = [p['Fitness'] for p in population]
@@ -114,11 +175,22 @@ def main() :
     infosFitnesses.append([bestFit, meanFit, worstFit, stdFit])
     print('Gen ', 0, ': Best: ', bestFit, ' Mean: ', meanFit, ' Worst:', worstFit, 'Std: ', stdFit)
 
+    p_croisement = 0.5 # proportion of croisement, 1-p_croisement the proportion of mutatoion
+    p_enfant = 0.5 # p_enfant * popSize the number of children produced
     
     for it in range(n_gen): # un tour de boucle = une génération
-        poplulation = nextGeneration(population)
-        
-	
+        # 3 ways to generate the next generation
+        poplulation = nextGeneration_1(population, popSize, p_croisement, p_enfant)
+        population = sorted(population, key=lambda k:k['Fitness'])
+        fitnesses = [p['Fitness'] for p in population]
+        infosFitnesses = []
+        bestFit = fitnesses[0]
+        meanFit = np.mean(fitnesses)
+        worstFit = fitnesses[-1]
+        stdFit = np.std(fitnesses)
+        infosFitnesses.append([bestFit, meanFit, worstFit, stdFit])
+        print('Gen ', it+1, ': Best: ', bestFit, ' Mean: ', meanFit, ' Worst:', worstFit, 'Std: ', stdFit)
+    fitnessRobot(population, True)
 	return 0
 
 if __name__ == "__main__" :
